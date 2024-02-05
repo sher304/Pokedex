c87363d83bdb1dc9ece3b0dc36c16ddf88a43e78
sher304 <esherovshermat@gmail.com>
2024-02-05 03:01:16 +0100

    no message

---
+++ SwiftUI/ChallangeApp/ChallangeApp/ChallangeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallangeAppApp.swift
+//  ChallangeApp
+//
+//  Created by Шермат Эшеров on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ChallangeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
